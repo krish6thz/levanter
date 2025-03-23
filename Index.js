dc14680ac84ffeec0cf376020c4543ff65ac931d
krish6thz <krish6thz@gmail.com>
2025-03-23 15:07:02 +0530

    index.js

---
+++ Index.js
@@ -0,0 +1,38 @@
+  } else {
+    // console.log('All dependencies are installed properly.')
+  }
+}
+
+function cloneRepository() {
+  // console.log('Cloning the repository...')
+  const cloneResult = spawnSync(
+    'git',
+    ['clone', 'https://github.com/lyfe00011/levanter.git', 'levanter'],
+    {
+      stdio: 'inherit',
+    }
+  )
+
+  if (cloneResult.error) {
+    throw new Error(`Failed to clone the repository: ${cloneResult.error.message}`)
+  }
+
+  const configPath = 'levanter/config.env'
+  try {
+    // console.log('Writing to config.env...')
+    writeFileSync(configPath, `VPS=true\nSESSION_ID=${SESSION_ID}`)
+  } catch (err) {
+    throw new Error(`Failed to write to config.env: ${err.message}`)
+  }
+
+  installDependencies()
+}
+
+if (!existsSync('levanter')) {
+  cloneRepository()
+  checkDependencies()
+} else {
+  checkDependencies()
+}
+
+startPm2()
